0e5fd7ff71ad5813a77dac8af408e61df66bb0ff
Parv Mehta <mehtaparv21@gmail.com>
2024-09-25 22:59:35 +0530

    Add logic of Connectted & Disconnected and started writing logic of move.

--- app.js
+++ app.js
@@ -11,18 +11,48 @@ const server = http.createServer(app);
 const io = socket(server);
 
 const chess = new Chess();
-let player = {};
-let currentPlayer = "W";
+let players = {};
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index", {title: "My Chess.com"});
+  res.render("index", { title: "My Chess.com" });
 });
 
-io.on("connection", function(uniquesocket){
-    console.log("Connected");
+io.on("connection", function (uniquesocket) {
+  console.log("Connected");
+
+  if (!players.white) {
+    players.white = uniquesocket.id;
+    uniquesocket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = uniquesocket.id;
+    uniquesocket.emit("playerRole", "b");
+  } else {
+    uniquesocket.emit("spectatorRole");
+  }
+
+  socket.on("disconnect", function () {
+    console.log("Disconnected");
+    if (uniquesocket.id === players.white) {
+      delete players.white;
+    } else if (uniquesocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  uniquesocket.on("move", (move) => {
+    try {
+      if (chess.turn() === "w" && uniquesocket.id === players.white) {
+        return;
+      }
+      if (chess.turn() === "b" && uniquesocket.id === players.black) {
+        return;
+      }
+    } catch (error) {}
+  });
 });
 
 server.listen(3000, function () {
